99aec467155972c953e3a73fd381e56f83a447e8
D7608 <D7608@DESKTOP-PNB790R>
2018-11-29 10:16:32 +0900

    DownloadDB

---
+++ 201412377-BoardDownloadDB/src/main/java/kr/ac/dit/persistence/BoardDAO.java
@@ -0,0 +1,16 @@
+package kr.ac.dit.persistence;
+import java.util.*;
+import kr.ac.dit.domain.BoardVO;
+import kr.ac.dit.domain.UploadFileVO;
+public interface BoardDAO {
+	public List<BoardVO> selectList() throws Exception;
+	public void insert(BoardVO boardVO) throws Exception;
+	public BoardVO select(int no) throws Exception;
+	public void update(BoardVO boardVO) throws Exception;
+	public void delete(int no) throws Exception;
+	public void insertFile(UploadFileVO uploadFileVO);
+	public int selectMaxFileId();
+	public List<UploadFileVO> selectAttachFile(int no);
+	public UploadFileVO selectDownloadFile(int fileId);
+	
+}
